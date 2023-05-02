3f8e107550774756ad85a3c053f096d80147d3d4
Badro1859 <badromidani@gmail.com>
2023-05-02 15:08:07 +0000

    version 1.0

--- ipfs/init-config.sh
+++ ipfs/init-config.sh
@@ -2,3 +2,5 @@
 set -ex
 ipfs bootstrap rm all
 ipfs config Routing.Type dht
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST", "GET"]'
